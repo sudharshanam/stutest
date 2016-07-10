f2a650b10e86556971494bf00cb2f0ce1d152819
sudharshanam <dharanikumard14@gmail.com>
2016-07-10 12:55:06 +0530

    Update and rename tree.py to tree.c

---
+++ tree.c
@@ -0,0 +1,34 @@
+#stutest
+#include<iostream.h>
+#include<stdio.h>
+void main()
+{
+char name={"TREE"};
+int thick;
+int branch;
+char color[];
+int a;
+printf("enter the thickness of the tree");
+scanf("%d,&thick");
+printf("enter the branches of the tree");
+scanf("%d,branch");
+printf("enter the color of the tree");
+scanf("%c,&color ");
+printf("enter the angle of the tree");
+scanf("%d,&a");
+if(thick>23)
+{
+if (branch>20)
+{
+if(a<=45)
+{
+printf("\n the angle of the tree is:");
+}
+printf("\n the branches of the tree is:");
+}
+printf("\n the thickness of the tree is:");
+}
+getch()
+}
+   
+
