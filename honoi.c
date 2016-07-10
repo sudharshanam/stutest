cbc6f2f95978722fe8892a2a811dcd11042f85f4
sudharshanam <dharanikumard14@gmail.com>
2016-07-10 11:29:41 +0530

    Create honoi.c

---
+++ honoi.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+void Hanoitower(int,char,char,char);
+void Hanoitower(int n, char frod, char torod, char aurod)
+{
+    if (n == 1)
+    {
+        printf("\n Move disk 1 from rod %c to rod %c", frod, torod);
+        return;
+    }
+    towerOfHanoi(n-1, frod, aurod, torod);
+    printf("\n Move disk %d from rod %c to rod %c", n, frod, torod);
+    towerOfHanoi(n-1, aurod, torod, frod);
+    getch();
+}
+ 
+int main()
+{
+    int n = 3; // Number of disks
+    Hanoitower(n, 'A', 'C', 'B');  // A, B and C are names of rods
+    return 0;
+}
