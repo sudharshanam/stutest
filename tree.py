a828bf83e9802e231f2e6245e91f689534c21b87
sudharshanam <dharanikumard14@gmail.com>
2016-07-10 12:32:25 +0530

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,22 +1,22 @@
 #stutest
 import turtle
 def tree(branchLen,t):
-if branchLen > 5:
+if branchLen > 3:
 t.forward(branchLen)
-t.right(20)
+t.right(40)
 tree(branchLen-15,t)
-t.left(40)
+t.left(50)
 tree(branchLen-15,t)
-t.right(20)
+t.right(40)
 t.backward(branchLen)
 def main():
 t = turtle.Turtle()
 myWin = turtle.Screen()
-t.left(90)
+t.left(80)
 t.up()
 t.backward(100)
 t.down()
-t.color("green")
+t.color("brown")
 tree(75,t)
 myWin.exitonclick()
 main()
